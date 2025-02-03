0e16ed91f6915e7faa2c38c2a8f1e7e6e5f31cdb
EdwinChuaXZ <ecxzhi@gmail.com>
2025-02-02 21:23:50 -0600

    TheTriangleFarmer
    TheTriangleFarmer

---
+++ Class1.cs
@@ -0,0 +1,56 @@
+﻿// Written by Edwin
+// 2/1/2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheTriangleFarmer
+{
+    public class Triangle
+    {
+        //Value to be entered
+        private double tbase = -1.00;
+        private double theight = -1.00;
+
+        //get and set
+        public double tBase 
+        {
+            get { return this.tbase; }
+            set { this.tbase = value; }
+
+        }
+
+        public double tHeight
+        {
+            get { return this.theight; }
+            set { this.theight = value; }
+
+        }
+
+        public double tArea() // Triangle calculation
+        {
+
+            double tarea = -1.00;
+
+            tarea = (this.tBase * this.tHeight) / 2;
+
+
+            return tarea;
+        }
+
+        public override string ToString()//override
+        {
+            String message = "";
+
+            message += "The base of a triangle is : " + this.tBase + "\n";
+            message += "The height of a triangle is : " + this.tHeight + "\n";
+            message += "The area of a triangle is : " + this.tArea() + "\n";
+            
+            return message;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// Written by Edwin
+// 2/1/2025
+
+namespace TheTriangleFarmer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Triangle aTriangle = new Triangle();
+
+
+
+
+                Console.WriteLine("Enter the base of the triangle:");
+                double atbase = Convert.ToDouble(Console.ReadLine());//the base of the triangle.
+
+
+                if (atbase > 0) { aTriangle.tBase = atbase; } //if the number is correct
+                else { Console.WriteLine("Please enter the correct number." + "\n" + "\n"); continue; }//If the number is less than 0, display an error and run again
+
+                Console.WriteLine("Enter the height of the triangle");
+                double atheight = Convert.ToDouble(Console.ReadLine());
+
+                if (atheight > 0) { aTriangle.tHeight = atheight; }// if the number is correct
+                else { Console.WriteLine("Please enter the correct number."+"\n" + "\n"); continue; }//If the number is less than 0, display an error and run again
+
+                Console.WriteLine(aTriangle.ToString()); //If there are no problems, the results will be displayed.
+
+                Console.WriteLine("Do you want to continue? (Y or N) " + "\n"+"\n");//Whether to continue running.
+                string? choose = Console.ReadLine();
+
+                if (choose == "Y" || choose =="y") { continue; }//If so, run again
+
+                else if (choose == "N" || choose =="n") { Console.WriteLine("End of program"); break; }//If not, stop
+
+                else { Console.WriteLine("Input error, end of program");break; }//Input error, stop
+
+            }
+        }
+    }
+}
