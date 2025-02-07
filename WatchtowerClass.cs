f8e1db30bd39d6bf58442fd1cba84c645ce07607
EdwinChuaXZ <ecxzhi@gmail.com>
2025-02-07 02:27:24 -0600

    Homework 2
    Homework 2

---
+++ WatchtowerClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watchtower
+{
+    public class WatchtowerClass
+    {
+        //user input
+        private int x = -1;
+        private int y = -1;
+
+        //get and set
+        public int GetX { get { return this.x; } set { this.x = value; } }
+        public int GetY { get { return this.y; } set { this.y = value; } }
+        // determine Coordinates, if function
+        public string Coordinates()
+        {
+            string coordinates = "n/a";
+
+                if(x<0 && y>0){ coordinates = "NW"; }
+                else if(x<0 && y==0){ coordinates = "W"; }
+                else if (x < 0 && y < 0) { coordinates = "SW"; }
+                else if (x == 0 && y > 0) { coordinates = "N"; }
+                else if (x == 0 && y == 0) { coordinates = "!"; }
+                else if (x == 0 && y < 0) { coordinates = "S"; }
+                else if (x > 0 && y > 0) { coordinates = "NE"; }
+                else if (x > 0 && y == 0) { coordinates = "E"; }
+                else if (x > 0 && y < 0) { coordinates = "SE"; }
+
+                return coordinates;}
+
+         //override
+         public override string ToString()
+{
+
+             string message = "";
+
+              if (Coordinates() == "!") { message += "The enemy is here!"+ "\n"; }
+              
+              else { message += "The enemy is to the " +Coordinates() + "!"; }
+
+               return message;
+
+
+
+}
+
+    }
+}
---
+++ WatchtowerProgram.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Watchtower
+{
+    internal class WatchtowerProgram
+    {
+        public static void Main(string[] args)
+        {
+            WatchtowerClass aWatchtowerClass = new WatchtowerClass();
+
+            // input x and y variable
+
+            Console.WriteLine("Pls enter the coordinates: ");
+            Console.WriteLine("X values: ");
+            aWatchtowerClass.GetX = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Y values: ");
+            aWatchtowerClass.GetY = Convert.ToInt32(Console.ReadLine());
+
+            //output result
+            Console.WriteLine(aWatchtowerClass.ToString());
+
+        }
+    }
+}
