5322c5e6dd43efff286b21038c165cd1d3436510
EdwinChuaXZ <ecxzhi@gmail.com>
2025-02-07 04:49:23 -0600

    Homework 2
    Homework 2

---
+++ ThePrototypeClass1.cs
@@ -0,0 +1,50 @@
+﻿//Writting by Edwin
+//2/6/2025
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Prototype
+{
+    public class ThePrototypeClass1
+    {
+        private int pilot = -1;
+        private int hunter = -1;
+
+        //get and set
+        public int Pilot { get { return this.pilot; } set { this.pilot = value; } }
+        public int Hunter { get { return this.hunter; } set { this.hunter = value; } }
+
+        //override
+        public override string ToString()
+        {
+            string message = "";
+
+            
+
+                if (Pilot == Hunter)
+                {
+                    message += "You guesed the number!"; 
+
+                }
+                else if (Hunter > Pilot)
+                {
+                    message += Hunter + "is too high!"; 
+                }
+                else if (Hunter < Pilot)
+                {
+                    message += Hunter + "is too low!";
+                }
+
+                else { message += "Iput error"; }
+
+               
+
+
+            return message;
+        }
+    }
+}
---
+++ ThePrototypeProgram.cs
@@ -0,0 +1,71 @@
+﻿//Writting by Edwin
+//2/6/2025
+using System.Diagnostics.Metrics;
+
+namespace The_Prototype
+{
+    internal class ThePrototypeProgram
+    {
+        static void Main(string[] args)
+        {
+            ThePrototypeClass1 aThePrototypeClass1 = new ThePrototypeClass1();
+
+            //menu:
+            // output and input
+
+
+            while (true)
+            {
+                // if pilot input more tha the range, run again
+                Console.WriteLine("User1, enter a number between 0 and 100");
+                int apilot = Convert.ToInt32(Console.ReadLine());
+
+                if (apilot >= 0 && apilot <= 100)
+                {
+                    aThePrototypeClass1.Pilot = apilot; break;
+                }
+
+
+                else { Console.WriteLine("Input Error!"); continue; }
+
+            }
+
+            while (true)
+            {
+                 // hunter input
+                Console.WriteLine("User2 :");
+                int ahunter = Convert.ToInt32(Console.ReadLine());
+
+                aThePrototypeClass1.Hunter = ahunter;
+
+                //if correct , end program
+                if (ahunter == aThePrototypeClass1.Pilot)
+                {
+                    
+
+
+                    Console.WriteLine(aThePrototypeClass1.ToString()); break;
+
+
+                }
+
+                //if not, ask again
+                else if (ahunter > aThePrototypeClass1.Pilot)
+                { 
+                    Console.WriteLine(aThePrototypeClass1.ToString()); continue;
+                }
+                else
+                { Console.WriteLine(aThePrototypeClass1.ToString()); continue; }
+
+
+            }
+
+        }
+                   
+
+                
+            
+
+        }
+    }
+
